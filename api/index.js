96d1733674ca452d65534f17cf147bd3309b53be
sureshDuminda94 <114845061+sureshDuminda94@users.noreply.github.com>
2024-02-17 08:10:10 -0600

    create api and routes

--- api/index.js
+++ api/index.js
@@ -1,11 +1,17 @@
 import express from "express";
-
+//https://www.youtube.com/watch?v=Kkht2mwSL_I
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRoutes from './routes/user.routes.js'
 const app = express();
+dotenv.config();
+mongoose.connect("mongodb://127.0.0.1:27017/my-blog").then(() => {
+  console.log("ok");
+});
 
 app.listen(3000, () => {
-
-
-            console.log('server is runnig on port 3000')
-
-
+  console.log("server is runnig on port 3000");
 });
+
+
+app.use("/user",userRoutes)
---
+++ api/model/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    require: true,
+
+  },
+
+
+
+
+});
